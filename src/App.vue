0be1d4a9bb5d111c6a9151b3e3bdbb3034df0c0f
dannesx <daniel.viantunes@gmail.com>
2025-11-28 19:45:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useColorMode } from "@vueuse/core"
+import SelectPriority from "./components/SelectPriority.vue"
+import { PlusCircle } from "lucide-vue-next"
+
+const mode = useColorMode()
+mode.value = "dark"
+</script>
+
+<template>
+  <div class="max-w-3xl mx-auto p-4">
+    <h1>Tasks ✨</h1>
+    <p class="mb-2">
+      A simple task manager made with <code>Vue.js</code> &
+      <code>Laravel PHP</code>
+    </p>
+
+    <div class="flex gap-2">
+      <Input placeholder="Add task" class="flex-1" />
+      <SelectPriority />
+      <Button>
+        <PlusCircle />
+        Add task
+      </Button>
+    </div>
+  </div>
+</template>
---
+++ src/components/SelectPriority.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ArrowDown, ArrowUp, Minus } from "lucide-vue-next"
+import { Select, SelectTrigger, SelectContent, SelectItem } from "./ui/select"
+import SelectValue from "./ui/select/SelectValue.vue"
+</script>
+
+<template>
+  <Select>
+    <SelectTrigger>
+      <SelectValue placeholder="Priority" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem value="1">
+        <ArrowUp />
+        High
+      </SelectItem>
+      <SelectItem value="2">
+        <Minus />
+        Medium
+      </SelectItem>
+      <SelectItem value="3">
+        <ArrowDown />
+        Low
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
